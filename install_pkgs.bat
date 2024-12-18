1d1ab349514a5f49a3c7812f473fa789c565c31c
Siddharth Singh <37984218+sid060594@users.noreply.github.com>
2024-12-18 17:23:39 -0600

    initial commit

---
+++ install_pkgs.bat
@@ -0,0 +1,16 @@
+:: ELASTIC SEARCH GUI by SIDDHARTH SINGH (siddharth_singh) on 06/28/2020
+@echo off
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+echo Please wait while the necessary packages get installed...
+pip install tkcalendar
+pip install xlsxwriter
+pip install elasticsearch
+echo RUNNING ELASTIC SEARCH GUI...
+
+cd=%cd%
+
+cd %cd%\code
+
+python ui.py
+
+exit
